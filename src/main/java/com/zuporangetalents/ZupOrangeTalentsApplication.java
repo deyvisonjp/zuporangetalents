84556506744a2fcd0cba15a40b0c8207fe048879
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-12-25 17:14:15 -0300

    Configuração inicial

---
+++ src/main/java/com/zuporangetalents/ZupOrangeTalentsApplication.java
@@ -0,0 +1,13 @@
+package com.zuporangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupOrangeTalentsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ZupOrangeTalentsApplication.class, args);
+	}
+
+}
